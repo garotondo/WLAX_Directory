f7f83827407bca36420b5dfeafeced90b9cafbcf
Grace Rotondo <gracerotondo@dhcp-10-250-174-133.harvard.edu>
2019-12-02 23:35:12 -0500

    app done for demo day

--- directory_app/app.R
+++ directory_app/app.R
@@ -46,50 +46,54 @@ ui <- fluidPage(
                
                #The third tab is the About page.
                tabPanel("About",
-                        titlePanel(h3("Fixing the Flaws of Networking: Creating an Accurate Alumni Directory for the Harvard Women's Lacrosse Program")),
+                        titlePanel(h2("Fixing the Flaws of Networking: An Accurate Alumni Directory of the Harvard Women's Lacrosse Program")),
                         
                         hr(),
                         h4("Overview"),
-                        h5("'If your not Networking, you're doing Harvard wrong.' - David Kane"),
-                        h5("Networking is one of the most important skills an individual can develop at Harvard. 
-                            Though academics are important, the people you know are what is going to help you ultimately 
-                            land a job and launch a career after college. However this is the case, I found in my own job 
-                            search that Harvard's alumni resources are out-of-date and unreliable. To solve this problem, 
-                            I set out to develop an alumni directory - for the Harvard Women's Lacrosse Program."),
+                        p("'If your not Networking, you're doing Harvard wrong.' - David Kane"),
+                        p("Networking is one of the most important skills a person can develop while at Harvard. 
+                           Though academics are important, your network of people is what is going to help you 
+                           ultimately land a job and launch a career after college. Though this is the case, I 
+                           found in my own job search that Harvard's alumni resources are out-of-date and unreliable. 
+                           To solve this problem, I set out to develop an alumni directory - for the Harvard Women's 
+                           Lacrosse Program. "),
                                       
                         # The br() function adds white space to the app.
                         # Data Collection Explanation
                         br(),
                         h4("Data Collection"), 
-                        h5("To develop the directory, I used five sources of data: 1) an excel spreadsheet from 
-                            the Harvard Varsit Club that included contact information; 2) Names of the Varsity 
-                            Letterwinners of Harvard Women's Lacrosse; 3) House and Concentration information from 
-                            the official Harvard Alumni Directory; 4) Information from LinkedIn; and 5) georgraphic 
-                            locations of US cities. The second, third, and fourth sources I personally researched and 
-                            collected their data to put in an excel spreadsheet. I especially wanted to manually search 
-                            individuals' LinkedIn accounts to ensure that they were accurate matches so that if the given 
-                            emails (by the Varsity Club) were not accurate, then users would have the option to reach out 
-                            to someone through LinkedIn."),
+                        p("To develop the directory, I used five sources of data: 1) an excel spreadsheet from the 
+                        Harvard Varsity Club that included contact information of alums listed in the Varsity Club's 
+                        records; 2) Names of the Varsity Letterwinners of Harvard Women's Lacrosse, from 
+                        <https://www.gocrimson.com/sports/wlax/history/letterwinners>; 3) House and Concentration 
+                        information from the official Harvard Alumni Directory; 4) LinkedIn profiles; and 5) Coordinates 
+                        of US cities to be used for the map, from <https://simplemaps.com/data/us-cities>. I personally 
+                        researched and collected the data from the second, third, and fourth sources to compile my own 
+                        excel spreadsheet, named *wlax*. I wanted to manually research individuals' LinkedIn accounts 
+                        to include in *wlax* to ensure that they were accurate matches to the alum profiles so that if 
+                        the given emails (by the Varsity Club) were not accurate, users would have the option to reach 
+                        out to an alum through LinkedIn. After manually collecting the data, I cleaned it and joined it 
+                        to the Varsity Club dataset. From there, I joined the US cities' coordinates data to the full 
+                        alumni dataset to create my full dataset."),
                         br(),
-                                      
-                        # Search Page Explanation
-                        h4("Data Use: The Directory"),
-                        h5("I used the data to create an interface, which was designed to allow for individuals to search 
-                            women's lacrosse alums by Name (full, first, and last), Industry, and Concentration. The 
-                            dashboard includes an interactive search table for the name search and drop down options for 
-                            the Industry and Concentration searches"),
-
-                        br(),
+                        
                         # Map Page Explanation
                         h4("Data Use: Interactive Map"),
-                        h5("This data was used to create an interactive map in which individuals could search locations 
-                            where alums resided based on their LinkedIn profiles. This is where the locations data was 
-                            necessary to match the cities in the alumnae data with proper coordinates so that the cities 
-                            could be located on the map."),
+                        p("This data was used to create an interactive map using Leaflet in which individuals could find 
+                        the home locations of alums. I then added points that indicate the alums' home locations, and 
+                        Leaflet allows for a user to click on a point and see who is from that particular location."),
+                        br(),
+                        
+                        # Search Page Explanation
+                        h4("Data Use: The Directory"),
+                        p("I used the data to create an interface, which was designed to allow for individuals to search 
+                           women's lacrosse alumni by any term they would like. The dashboard includes an interactive search 
+                           table created with DT, allows for people to input whatever terms they would like into the search bar, 
+                           and produces any rows of information containing the relevant search term."),
                         br(),
 
-                        h4("About Me: Grace Rotondo"),
-                        h5("I am a junior at Harvard College studying Psychology and Economics. Additionally, I am a 
+                        h5("About Me: Grace Rotondo"),
+                        p("I am a junior at Harvard College studying Psychology and Economics. Additionally, I am a 
                             member of the Women's Lacrosse Team, The Student-Athlete Advisory Committee, and Harvard 
                             Undergraduate Women in Business. Contact me at: grotondo@college.harvard.edu."),
                         br(),
