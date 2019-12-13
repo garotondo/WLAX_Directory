8893973aff09a46e109199cb8754e1e5e632cd31
Grace Rotondo <gracerotondo@dhcp-10-250-241-118.harvard.edu>
2019-12-13 08:44:39 -0500

    Final Commit

--- directory_app/pdf_prep.R
+++ directory_app/pdf_prep.R
@@ -1,3 +1,4 @@
-
 library(fs)
-file_copy("directory_app/writeup.pdf", "directory_app/www")
+file_copy("/Users/gracerotondo/Desktop/GOV1005\ /Project/WLAX_Directory/directory_app/writeup.pdf",
+          "/Users/gracerotondo/Desktop/GOV1005\ /Project/WLAX_Directory/directory_app/www")
+
